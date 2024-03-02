b2ae84fbebf59d3ceef77c994cb7171e24acd71b
hautran7201 <tranhoanghau7201@gmail.com>
2024-03-02 19:02:15 +0000

    update

--- augmentation.py
+++ augmentation.py
@@ -20,6 +20,25 @@ tokenizer = MBart50TokenizerFast.from_pretrained(
 
 
 # Create model
+inputs = self.tokenizer(
+      text,
+      padding='max_length',
+      truncation=True,
+      max_length=75,
+      return_tensors='pt'
+)
+inputs = self.accelerator.prepare(inputs)
+
+
+# Generate
+outputs = self.accelerator.unwrap_model(self.model).generate(
+    inputs.input_ids,
+    attention_mask = inputs.attention_mask,
+    max_new_tokens = 75,
+    num_beams = 4
+)
+output_str = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
 v2e_model = TranslationModel(model, tokenizer, accelerator=accelerator)
 
 
@@ -35,9 +54,27 @@ vi_phml = vi_phml
 en_phomt_preds = []
 process_bar = tqdm(range(len(vi_phml)))
 for sent in vi_phml:
-    output = v2e_model.inference(sent)
+    # Tokenize
+    inputs = self.tokenizer(
+          text,
+          padding='max_length',
+          truncation=True,
+          max_length=75,
+          return_tensors='pt'
+    )
+    inputs = self.accelerator.prepare(inputs)
+
+    # Generate
+    outputs = self.accelerator.unwrap_model(self.model).generate(
+        inputs.input_ids,
+        attention_mask = inputs.attention_mask,
+        max_new_tokens = 75,
+        num_beams = 4
+    )
+    output = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)    
     en_phomt_preds.append(output[0])
     process_bar.update(1)
+
 
 # Convert to dataset
 aug_data = Dataset.from_dict(
